49073f3080a68e3ef4a8c160af20a817d3525894
ruslanm7778 <ruslanM7778@gmail.com>
2020-08-05 12:34:02 +0300

    Updates

---
+++ precache-manifest.f13e7732956606de091d222e3af8bd23.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b6c5d85224a56c6656f1a9befc35905",
+    "url": "/react-app-Pogoda/index.html"
+  },
+  {
+    "revision": "b593f37e905a3f44bb9f",
+    "url": "/react-app-Pogoda/static/css/main.d13920b9.chunk.css"
+  },
+  {
+    "revision": "e0a94a14601ede8c3ce5",
+    "url": "/react-app-Pogoda/static/js/2.58da5a1b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app-Pogoda/static/js/2.58da5a1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b593f37e905a3f44bb9f",
+    "url": "/react-app-Pogoda/static/js/main.62799f33.chunk.js"
+  },
+  {
+    "revision": "a0d4d008135725c4c331",
+    "url": "/react-app-Pogoda/static/js/runtime-main.fd707b36.js"
+  },
+  {
+    "revision": "cc54c19b29890841267ee0750eb25a00",
+    "url": "/react-app-Pogoda/static/media/polygon.cc54c19b.jpg"
+  }
+]);
